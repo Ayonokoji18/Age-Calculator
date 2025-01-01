ec1d0e5ad01944b0b22b4cba11c74b0307189daf
PaWAn ChAuDhArY <166200693+Ayonokoji18@users.noreply.github.com>
2025-01-02 01:16:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const btnEl = document.getElementById("btn");
+const birthdayEl = document.getElementById("birthday");
+const resultEl = document.getElementById("result");
+
+function calculateAge() {
+  const birthdayValue = birthdayEl.value;
+  if (birthdayValue === "") {
+    alert("Please enter your birthday ");
+  } else {
+    let age = getAge(birthdayValue);
+    resultEl.innerText = `Your age is ${age} ${
+      age > 1 ? "years" : "year"
+    } old Boss`;
+  }
+}
+
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  console.log(age);
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+  return age;
+}
+
+btnEl.addEventListener("click", calculateAge);
